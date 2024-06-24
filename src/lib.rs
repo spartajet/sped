a3a3af972120f8f050bdede3643fd1a6eb6c9620
Dr.Guo <guoxiaozhong1990@outlook.com>
2024-06-24 21:27:36 +0800

    Refactored edge detection algorithm and added unit tests
    - Refactored the edge detection algorithm to improve readability and performance
    - Added unit tests for edge detection algorithm and usize conversion
    - Updated dependencies in Cargo.toml for opencv to version 0.92.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,41 @@
 use std::error::Error;
-use std::f64::EPSILON;
-use opencv::core::Point2d;
-use opencv::prelude::{Mat, MatTraitConst};
+use opencv::core::{Size, Vector};
+use opencv::imgcodecs::imwrite;
+use opencv::prelude::Mat;
 
 pub fn devernay(image: &Vec<u8>, rows: usize, cols: usize, sigma: f64, th_h: f64, th_l: f64) -> Result<DevernayResult, Box<dyn Error>> {
     let pixel_num = rows * cols;
-    let mut gx = vec![0f64; pixel_num as usize];
-    let mut gy = vec![0f64; pixel_num as usize];
-    let mut mod_g = vec![0f64; pixel_num as usize];
+    let mut gx = vec![0f64; pixel_num];
+    let mut gy = vec![0f64; pixel_num];
+    let mut mod_g = vec![0f64; pixel_num];
     if sigma == 0f64 {
         compute_gradient(image, rows, cols, &mut gx, &mut gy, &mut mod_g);
     } else {
         let gauss = gaussian_filter(image, rows, cols, sigma)?;
+        let gauss_array = gauss.as_slice();
+        let gauss_mat = Mat::new_size_with_data(Size::new(cols as i32, rows as i32), gauss_array).unwrap();
+        imwrite("resources/gauss.png", &gauss_mat, &Vector::<i32>::new()).unwrap();
+        for (i, v) in gauss.iter().enumerate() {
+            if (*v < 255) {
+                // println!("i: {}, v: {}", i, v);
+            }
+        }
         compute_gradient(&gauss, rows, cols, &mut gx, &mut gy, &mut mod_g);
+    }
+    for i in 0..pixel_num {
+        if gx[i] == 0f64 || gy[i] == 0f64 || mod_g[i] == 0f64 { continue; }
+        // println!("i:{} gx: {:.4}, gy: {:.4}, mod_g: {:.4}", i, gx[i], gy[i], mod_g[i]);
     }
     let (edge_x, edge_y) = compute_edge_points(&gx, &gy, &mod_g, rows, cols)?;
     let (mut next, mut prev) = chain_edge_points(&edge_x, &edge_y, &gx, &gy, rows, cols)?;
     thresholds_with_hysteresis(&mut next, &mut prev, &mod_g, rows, cols, th_h, th_l)?;
-
-    let mut result = DevernayResult::default();
+    let (xs, ys, curve_limits, curves_num) = list_chained_edge_points(&mut next, &mut prev, &edge_x, &edge_y, rows, cols)?;
+    let result = DevernayResult {
+        xs,
+        ys,
+        curve_limits,
+        curves_num,
+    };
     Ok(result)
 }
 
@@ -28,7 +45,7 @@ fn list_chained_edge_points(next: &mut Vec<i32>, prev: &mut Vec<i32>, edge_x: &V
     let mut ys = vec![0f64; pixel_num];
     let mut curve_limits = vec![0i32; pixel_num];
     // let mut i = 0;
-    let mut k = 0;
+    // let mut k = 0;
     let mut n = 0i32;
     let mut nn = 0;
     let mut m = 0;
@@ -37,20 +54,20 @@ fn list_chained_edge_points(next: &mut Vec<i32>, prev: &mut Vec<i32>, edge_x: &V
             curve_limits[m] = n;
 
             m = m + 1;
-            k = i;
-            n = prev[k];
+            let mut k = i as i32;
+            n = prev[k as usize];
             while n >= 0 && n != i as i32 {
-                k = n as usize;
-                n = prev[k];
+                k = n;
+                n = prev[k as usize];
             }
             while k >= 0 {
-                xs[nn] = edge_x[k];
-                ys[nn] = edge_y[k];
+                xs[nn] = edge_x[k as usize];
+                ys[nn] = edge_y[k as usize];
                 nn = nn + 1;
-                n = next[k];
-                next[k] = -1;
-                prev[k] = -1;
-                k = n as usize;
+                n = next[k as usize];
+                next[k as usize] = -1;
+                prev[k as usize] = -1;
+                k = n;
             }
         }
         curve_limits[m] = nn as i32;
@@ -62,39 +79,39 @@ fn list_chained_edge_points(next: &mut Vec<i32>, prev: &mut Vec<i32>, edge_x: &V
 fn thresholds_with_hysteresis(next: &mut Vec<i32>, prev: &mut Vec<i32>, mod_g: &Vec<f64>, cols: usize, rows: usize, th_h: f64, th_l: f64) -> Result<(), Box<dyn Error>> {
     let pixel_num = rows * cols;
     let mut valid = vec![0; pixel_num];
-    let mut j = 0;
+    let mut j = 0i32;
     let mut k = 0;
     for i in 0..pixel_num {
         if (prev[i] >= 0 || next[i] >= 0) && valid[i] == 0 && mod_g[i] >= th_h {
             valid[i] = 1;
-            j = i;
-            k = next[j];
+            j = i as i32;
+            k = next[j as usize];
             while j >= 0 && k >= 0 && valid[k as usize] == 0 {
                 if mod_g[k as usize] < th_l {
-                    next[j] = -1;
+                    next[j as usize] = -1;
                     prev[k as usize] = -1;
                 } else {
                     valid[k as usize] = 1;
                 }
-                j = next[j] as usize;
+                j = next[j as usize];
             }
-            j = i;
-            k = prev[j];
+            j = i as i32;
+            k = prev[j as usize];
             while j >= 0 && k >= 0 && valid[k as usize] == 0 {
                 if mod_g[k as usize] < th_l {
                     next[k as usize] = -1;
-                    prev[j] = -1;
+                    prev[j as usize] = -1;
                 } else {
                     valid[k as usize] = 1;
                 }
-                j = prev[j] as usize;
+                j = prev[j as usize];
             }
         }
     }
     for i in 0..pixel_num {
         if (prev[i] >= 0 || next[i] >= 0) && valid[i] == 0 {
-            next[k as usize] = -1;
-            prev[j] = -1;
+            next[i] = -1;
+            prev[i] = -1;
         }
     }
     Ok(())
@@ -102,10 +119,10 @@ fn thresholds_with_hysteresis(next: &mut Vec<i32>, prev: &mut Vec<i32>, mod_g: &
 
 fn compute_gradient(image: &Vec<u8>, rows: usize, cols: usize, gx: &mut Vec<f64>, gy: &mut Vec<f64>, mod_g: &mut Vec<f64>) {
     // let mut i = 0;
-    for i in 1..cols {
-        for j in 1..rows {
-            gx[i + j * cols] = (image[(i + 1) + j * cols] - image[(i - 1) + j * cols]) as f64;
-            gy[i + j * cols] = (image[i + (j + 1) * cols] - image[i + (j - 1) * cols]) as f64;
+    for i in 1..cols - 1 {
+        for j in 1..rows - 1 {
+            gx[i + j * cols] = image[(i + 1) + j * cols] as f64 - image[(i - 1) + j * cols] as f64;
+            gy[i + j * cols] = image[i + (j + 1) * cols] as f64 - image[i + (j - 1) * cols] as f64;
             mod_g[i + j * cols] = (gx[i + j * cols].powi(2) + gy[i + j * cols].powi(2)).sqrt();
         }
     }
@@ -118,13 +135,13 @@ fn gaussian_filter(image: &Vec<u8>, rows: usize, cols: usize, sigma: f64) -> Res
     if rows < 1 || cols < 1 {
         return Err("Rows and cols must be positive".into());
     }
-    let len = (rows * cols);
+    let len = rows * cols;
     let mut out = vec![0; len];
     let mut tmp = vec![0f64; len];
     let prec = 3.0f64;
-    let offset = (sigma * (2f64 * prec * 10f64.log10()).sqrt()).ceil() as usize;
+    let offset = (sigma * (2f64 * prec * 10f64.ln()).sqrt()).ceil() as i32;
     let n = 2 * offset + 1;
-    let kernel = gaussian_kernel(n, sigma, offset as f64)?;
+    let kernel = gaussian_kernel(n as usize, sigma, offset as f64)?;
     let nx2 = 2 * cols;
     let ny2 = 2 * rows;
     // x axis convolution
@@ -132,17 +149,17 @@ fn gaussian_filter(image: &Vec<u8>, rows: usize, cols: usize, sigma: f64) -> Res
         for y in 0..rows {
             let mut value = 0f64;
             for i in 0..n {
-                let mut j = x - offset + i;
+                let mut j = x as i32 - offset + i;
                 while j < 0 {
-                    j = nx2 + j;
+                    j = nx2 as i32 + j;
                 }
-                while j > nx2 {
-                    j = j - nx2;
+                while j > nx2 as i32 {
+                    j = j - nx2 as i32;
                 }
-                if j > cols {
-                    j = nx2 - j - 1;
+                if j >= cols as i32 {
+                    j = nx2 as i32 - j - 1;
                 }
-                value += image[j + y * cols] as f64 * kernel[i];
+                value += image[j as usize + y * cols] as f64 * kernel[i as usize];
             }
             tmp[x + y * cols] = value;
         }
@@ -153,17 +170,17 @@ fn gaussian_filter(image: &Vec<u8>, rows: usize, cols: usize, sigma: f64) -> Res
         for y in 0..rows {
             let mut value = 0f64;
             for i in 0..n {
-                let mut j = y - offset + i;
+                let mut j = y as i32 - offset + i;
                 while j < 0 {
-                    j = ny2 + j;
+                    j = ny2 as i32 + j;
                 }
-                while j > ny2 {
-                    j = j - ny2;
+                while j > ny2 as i32 {
+                    j = j - ny2 as i32;
                 }
-                if j > rows {
-                    j = ny2 - j - 1;
+                if j >= rows as i32 {
+                    j = ny2 as i32 - j - 1;
                 }
-                value += tmp[x + j * cols] as f64 * kernel[i];
+                value += tmp[x + j as usize * cols] * kernel[i as usize];
             }
             out[x + y * cols] = value as u8;
         }
@@ -182,16 +199,16 @@ fn gaussian_kernel(n: usize, sigma: f64, mean: f64) -> Result<Vec<f64>, Box<dyn 
         return Err("n must be odd".into());
     }
     let mut sum = 0f64;
-    let mut kernel = vec![0f64; n as usize];
+    let mut kernel = vec![0f64; n];
     for i in 0..n {
         let val = (i as f64 - mean) / sigma;
         let tmp = (-0.5 * val.powi(2)).exp();
         sum += tmp;
-        kernel[i as usize] = tmp;
+        kernel[i] = tmp;
     }
     if sum > 0f64 {
         for i in 0..n {
-            kernel[i as usize] /= sum;
+            kernel[i] /= sum;
         }
     }
 
@@ -200,8 +217,8 @@ fn gaussian_kernel(n: usize, sigma: f64, mean: f64) -> Result<Vec<f64>, Box<dyn 
 
 fn compute_edge_points(gx: &Vec<f64>, gy: &Vec<f64>, mod_g: &Vec<f64>, rows: usize, cols: usize) -> Result<(Vec<f64>, Vec<f64>), Box<dyn Error>> {
     let pixel_num = rows * cols;
-    let mut edge_x = vec![-1f64; pixel_num as usize];
-    let mut edge_y = vec![-1f64; pixel_num as usize];
+    let mut edge_x = vec![-1f64; pixel_num];
+    let mut edge_y = vec![-1f64; pixel_num];
     for x in 2..cols - 2 {
         for y in 2..rows - 2 {
             let mut dx = 0;
@@ -213,12 +230,12 @@ fn compute_edge_points(gx: &Vec<f64>, gy: &Vec<f64>, mod_g: &Vec<f64>, rows: usi
             let mod_d = mod_g[x + (y - 1) * cols];
             let gx = gx[x + y * cols].abs();
             let gy = gy[x + y * cols].abs();
-            if greater_round(mod_value, mod_l) && !greater_round(mod_r, mod_value) && gx >= gy {
+            if mod_value > mod_l && mod_value > mod_r && gx >= gy {
                 dx = 1;
-            } else if greater_round(mod_value, mod_d) && !greater_round(mod_u, mod_value) && gx <= gy {
+            } else if mod_value > mod_d && mod_value > mod_u && gx <= gy {
                 dy = 1;
             }
-            if dx == 1 || dy == 1 {
+            if dx > 0 || dy > 0 {
                 let a = mod_g[x - dx + (y - dy) * cols];
                 let b = mod_g[x + y * cols];
                 let c = mod_g[x + dx + (y + dy) * cols];
@@ -236,7 +253,7 @@ fn chain_edge_points(edge_x: &Vec<f64>, edge_y: &Vec<f64>, gx: &Vec<f64>, gy: &V
     let pixel_num = rows * cols;
     let mut next = vec![-1i32; pixel_num];
     let mut prev = vec![-1i32; pixel_num];
-    let mut alt = 0i32;
+    // let mut alt = 0i32;
     for x in 2..cols - 2 {
         for y in 2..rows - 2 {
             if edge_x[x + y * cols] < 0f64 || edge_y[x + y * cols] < 0f64 {
@@ -249,8 +266,8 @@ fn chain_edge_points(edge_x: &Vec<f64>, edge_y: &Vec<f64>, gx: &Vec<f64>, gy: &V
             let mut bck = -1i32;
             for i in (-2..3).collect::<Vec<i32>>() {
                 for j in (-2..3).collect::<Vec<i32>>() {
-                    let to = (x + i as usize) + (y + j as usize) * cols;
-                    let s = chain(from, to, edge_x, edge_y, gx, gy, cols, rows)?;
+                    let to = (x as i32 + i) + (y as i32 + j) * cols as i32;
+                    let s = chain(from, to as usize, edge_x, edge_y, gx, gy, cols, rows)?;
                     if s > fwd_s {
                         fwd_s = s;
                         fwd = to as i32;
@@ -261,56 +278,90 @@ fn chain_edge_points(edge_x: &Vec<f64>, edge_y: &Vec<f64>, gx: &Vec<f64>, gy: &V
                     }
                 }
             }
-            alt = prev[fwd as usize];
-            let fwd_tmp = chain(alt as usize, fwd as usize, edge_x, edge_y, gx, gy, cols, rows)?;
-            if (fwd >= 0 && next[from] != fwd as i32 && (alt < 0 || fwd_tmp < fwd_s))
-            {
-                if (next[from] >= 0) {
-                    /* remove previous from-x link if one */
-                    prev[next[from] as usize] = -1;
-                } /* only prev requires explicit reset  */
-                next[from] = fwd as i32;         /* set next of from-fwd link          */
-                if (alt >= 0) {
-                    /* remove alt-fwd link if one         */
-                    next[alt as usize] = -1;
-                }   /* only next requires explicit reset  */
-                prev[fwd as usize] = from as i32;         /* set prev of from-fwd link          */
+            let mut alt = 0;
+            if fwd > 0 {
+                if next[from] != fwd {
+                    alt = prev[fwd as usize];
+                    let mut cal = false;
+                    if alt < 0 {
+                        cal = true;
+                    }else {
+                        let fwd_tmp = chain(alt as usize, fwd as usize, edge_x, edge_y, gx, gy, cols, rows)?;
+                        if fwd_tmp < fwd_s {
+                            cal = true;
+                        }
+                    }
+                    if cal {
+                        if next[from] >= 0 {
+                            /* remove previous from-x link if one */
+                            prev[next[from] as usize] = -1;
+                        } /* only prev requires explicit reset  */
+                        next[from] = fwd;         /* set next of from-fwd link          */
+                        if alt >= 0 {
+                            /* remove alt-fwd link if one         */
+                            next[alt as usize] = -1;
+                        }   /* only next requires explicit reset  */
+                        prev[fwd as usize] = from as i32;         /* set prev of from-fwd link          */
+                    }
+                }
             }
-            alt = next[bck as usize];
-            let bck_tmp = chain(alt as usize, bck as usize, edge_x, edge_y, gx, gy, cols, rows)?;
-            if (bck >= 0 && prev[from] != bck as i32 && (alt < 0 || bck_tmp > bck_s))
-            {
-                if (alt >= 0) {
-                    /* remove bck-alt link if one         */
-                    prev[alt as usize] = -1;
-                }      /* only prev requires explicit reset  */
-                next[bck as usize] = from as i32;         /* set next of bck-from link          */
-                if (prev[from] >= 0) {
-                    /* remove previous x-from link if one */
-                    next[prev[from] as usize] = -1;
-                } /* only next requires explicit reset  */
-                prev[from] = bck as i32;         /* set prev of bck-from link          */
+
+            if bck > 0 {
+                if prev[from] != bck {
+                    alt = next[bck as usize];
+                    let mut cal = false;
+                    if alt < 0 {
+                        cal = true;
+                    } else {
+                        let bck_tmp = chain(alt as usize, bck as usize, edge_x, edge_y, gx, gy, cols, rows)?;
+                        if bck_tmp > bck_s {
+                            cal = true;
+                        }
+                    }
+                    if cal{
+                        if alt >= 0 {
+                            /* remove bck-alt link if one         */
+                            prev[alt as usize] = -1;
+                        }      /* only prev requires explicit reset  */
+                        next[bck as usize] = from as i32;         /* set next of bck-from link          */
+                        if prev[from] >= 0 {
+                            /* remove previous x-from link if one */
+                            next[prev[from] as usize] = -1;
+                        } /* only next requires explicit reset  */
+                        prev[from] = bck;         /* set prev of bck-from link          */
+                    }
+                }
             }
         }
     }
     Ok((next, prev))
 }
 
-fn chain(from: usize, to: usize, edge_x: &Vec<f64>, edge_y: &Vec<f64>, gx: &Vec<f64>, gy: &Vec<f64>, cols: usize, rows: usize) -> Result<f64, Box<dyn Error>> {
-    let mut dx = 0f64;
-    let mut dy = 0f64;
+fn chain(from: usize, to: usize, edge_x: &Vec<f64>, edge_y: &Vec<f64>, gx: &Vec<f64>, gy: &Vec<f64>, _cols: usize, _rows: usize) -> Result<f64, Box<dyn Error>> {
+    // let mut dx = 0f64;
+    // let mut dy = 0f64;
     if from == to {
         return Ok(0f64);
     }
     if edge_x[from] < 0f64 || edge_y[from] < 0f64 || edge_x[to] < 0f64 || edge_y[to] < 0f64 {
         return Ok(0f64);
     }
-    dx = edge_x[to] - edge_x[from];
-    dy = edge_y[to] - edge_y[from];
+    let dx = edge_x[to] - edge_x[from];
+    let dy = edge_y[to] - edge_y[from];
     if (gy[from] * dx - gx[from] * dy) * (gy[to] * dx - gx[to] * dy) < 0f64 {
         return Ok(0f64);
     }
-    Ok(0f64)
+    let mut result = 0f64;
+    if gy[from] * dx - gx[from] * dy >= 0f64 {
+        result = 1f64 / dist(edge_x[from], edge_y[from], edge_x[to], edge_y[to]);
+    } else {
+        result = -1f64 / dist(edge_x[from], edge_y[from], edge_x[to], edge_y[to]);
+    }
+    Ok(result)
+}
+
+fn dist(x1: f64, y1: f64, x2: f64, y2: f64) -> f64 {
+    ((x1 - x2).powi(2) + (y1 - y2).powi(2)).sqrt()
 }
 
 fn greater_round(a: f64, b: f64) -> bool {
@@ -325,10 +376,10 @@ fn greater_round(a: f64, b: f64) -> bool {
 
 
 #[derive(Debug, Clone, Default)]
-struct DevernayResult {
-    xs: Vec<f64>,
-    ys: Vec<f64>,
-    edge_points_num: i32,
-    curves_num: i32,
-    curve_limits: i32,
+pub struct DevernayResult {
+    pub xs: Vec<f64>,
+    pub ys: Vec<f64>,
+    // pub edge_points_num: i32,
+    pub curves_num: i32,
+    pub curve_limits: Vec<i32>,
 }
